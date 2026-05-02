e5c3639f583af75e0164e83937ff9e2a23c57f24
Raghav Sharma <code.practice17@gmail.com>
2026-05-02 21:42:40 +0530

    Improve snake constructor

--- 2-terminal-snake-game/snake.cpp
+++ 2-terminal-snake-game/snake.cpp
@@ -1,9 +1,30 @@
 #include "snake.h"
+#include "terminal.h"
 
 
 Snake::Snake(const Region game_region):
-    m_region { game_region }
+    m_region { game_region },            // region where snake moves
+    m_direction { Move_direction::UP }   // default move direction
 {
     // TODO: Set current position of snake to the center
-    // TODO: set move direction to up
+
+    Point top_left { m_region.top_left };
+    Point bottom_right { m_region.bottom_right };
+
+    int region_center_x { (bottom_right.m_x - top_left.m_x) / 2 };
+    int region_center_y { (bottom_right.m_y - top_left.m_y) / 2 };
+
+    // Snake will initially have 3 points as its body.
+    // head will be at center with subsequent 2 parts below it
+    m_body.push_back({region_center_x, region_center_y});
+    m_body.push_back({region_center_x, region_center_y + 1});
+    m_body.push_back({region_center_x, region_center_y + 2});
+}
+
+
+void Snake::draw(const Terminal term)
+{
+    for (auto body_part : m_body) {
+        term.print("0", body_part);
+    }
 }
--- 2-terminal-snake-game/snake.h
+++ 2-terminal-snake-game/snake.h
@@ -4,6 +4,7 @@
 
 #include "point.h"
 #include "region.h"
+#include "terminal.h"
 
 #include <vector>
 
@@ -18,12 +19,13 @@ class Snake
 {
 public:
     Snake(const Region game_region);
+    void draw(const Terminal term); // TODO: Temporary method
 
 private:
     Region m_region {};            // game region where the snake moves
 
-    std::vector<Point> body {};    // body of the snake, head at first, tail at end
-    Move_direction direction {};   // direction in which snake is moving
+    std::vector<Point> m_body {};    // body of the snake, head at first, tail at end
+    Move_direction m_direction {};   // direction in which snake is moving
 };
 
 
